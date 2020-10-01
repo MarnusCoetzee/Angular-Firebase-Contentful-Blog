1768e9afceecfb929140d7f45803a77439308324
MarnusCoetzee <marnuscoe@gmail.com>
2020-10-01 16:35:13 +0200

    changes

--- src/app/services/contentful.service.ts
+++ src/app/services/contentful.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createClient, Entry } from 'contentful';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../environments/environment.prod';
 @Injectable({
   providedIn: 'root'
 })
